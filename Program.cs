c3d422e2910a098e17ec8b1a7f1416c35cf86c3d
Martin Honnen <martin.honnen@gmx.de>
2022-06-02 09:50:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Saxon.Api;
+
+var xsl1 = "sheet1.xsl";
+var xsl1Uri = new Uri(Path.Combine(Environment.CurrentDirectory, xsl1));
+
+Processor processor = new();
+
+XsltCompiler compiler = processor.NewXsltCompiler();
+compiler.BaseUri = xsl1Uri;
+
+XsltTransformer transformer = compiler.Compile(xsl1Uri).Load();
+
+transformer.SetParameter(new QName("input-uri"), new XdmAtomicValue("sample1.xml"));
+transformer.SetParameter(new QName("OS"), new XdmAtomicValue(Environment.OSVersion.ToString()));
+
+
+transformer.InitialTemplate = new QName("http://www.w3.org/1999/XSL/Transform", "initial-template");
+transformer.BaseOutputUri = xsl1Uri;
+
+// perform transformation
+XdmDestination result = new(); // effectively unused
+transformer.Run(result);
+
+var xsl2 = "sheet2.xsl";
+var xsl2Uri = new Uri(Path.Combine(Environment.CurrentDirectory, xsl2));
+
+
+compiler = processor.NewXsltCompiler();
+compiler.BaseUri = xsl2Uri;
+
+transformer = compiler.Compile(xsl2Uri).Load();
+
+transformer.SetParameter(new QName("input-uri"), new XdmAtomicValue("result1.xml"));
+transformer.SetParameter(new QName("OS"), new XdmAtomicValue(Environment.OSVersion.ToString()));
+
+
+transformer.InitialTemplate = new QName("http://www.w3.org/1999/XSL/Transform", "initial-template");
+transformer.BaseOutputUri = xsl2Uri;
+
+// perform transformation
+result = new(); // effectively unused
+transformer.Run(result);
---
+++ sheet2.xsl
@@ -0,0 +1,28 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="3.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:xs="http://www.w3.org/2001/XMLSchema"
+  xmlns:saxon="http://saxon.sf.net/"
+  expand-text="yes"
+  exclude-result-prefixes="#all">
+
+	<xsl:param name="input-uri" as="xs:string" required="yes"/>
+
+	<xsl:param name="OS" as="xs:string" required="yes"/>
+
+	<xsl:mode on-no-match="shallow-copy"/>
+
+	<xsl:template name="xsl:initial-template">
+		<xsl:source-document href="{$input-uri}">
+			<test>Run with {system-property('xsl:product-name')} {system-property('xsl:product-version')} on {$OS} at {saxon:timestamp()}</test>
+			<xsl:result-document href="result2.xml">
+				<xsl:apply-templates select="."/>
+			</xsl:result-document>
+		</xsl:source-document>
+	</xsl:template>
+
+	<xsl:template match="/">
+		<xsl:next-match/>
+		<xsl:comment>Run with {system-property('xsl:product-name')} {system-property('xsl:product-version')} on {$OS} at {saxon:timestamp()}</xsl:comment>
+	</xsl:template>
+
+</xsl:stylesheet>
